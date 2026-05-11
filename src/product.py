b6db26d424b466a532f3a07453b962588d4c3d9d
Kseniia Vakhterova <vlsen23@list.ru>
2026-05-11 21:29:12 +0300

    init commit

---
+++ src/product.py
@@ -0,0 +1,12 @@
+class Product:
+    """Класс продуктов"""
+    name: str
+    description: str
+    price: int
+    quantity: int
+
+    def __init__(self, name, description, price, quantity):
+        self.name = name
+        self.description = description
+        self.price = price
+        self.quantity = quantity
